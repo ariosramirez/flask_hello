757ead56c58bed6144cf8cf557ffc9a8dcfea636
Andres David Rios Ramirez <andres.riosramirez@mercadolibre.com>
2020-03-08 04:18:13 -0300

    added bootstrap into project

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 todos = ['Comprar café', 'ir a el banco', 'Compar fruta']
 
+@app.errorhandler(500)
+def not_found_500(error):
+    return render_template('500.html', error=error)
+
 @app.errorhandler(404)
-def not_found(error):
+def not_found_404(error):
     return render_template('404.html', error=error)
 
 
@@ -27,6 +32,11 @@ def hello_world():
         'todos': todos
     }
     return render_template('hello.html', **context)
+
+
+@app.route('/500_error_test')
+def index_pandas():
+    500
 
 
 if __name__ == '__main__':
