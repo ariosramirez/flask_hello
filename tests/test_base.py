6669140345dfe3a97c6e5ef3a4a58165bd9b31c6
Andres David Rios Ramirez <andres.riosramirez@mercadolibre.com>
2020-03-08 17:53:11 -0300

    added test redirects

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
 from flask_testing import TestCase
-from flask import current_app
+from flask import current_app, url_for
 
 from app import app
 
@@ -15,5 +15,6 @@ class MainTest(TestCase):
     def test_app_in_test_mode(self):
         self.assertTrue(current_app.config['TESTING'])
 
-    def test_index_redirect_to_hello(self):
-        pass
+    def test_index_redirects_to_hello(self):
+        response = self.client.get(url_for('index'))
+        self.assertRedirects(response, url_for('hello_world'))
