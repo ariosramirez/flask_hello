46082cea292db1c99ced2a28bd09227919760f6a
Andres David Rios Ramirez <andres.riosramirez@mercadolibre.com>
2020-03-09 23:12:53 -0300

    added test into app

--- app.py
+++ app.py
@@ -1,29 +1,22 @@
-from flask import (Flask,
-                   request,
-                   make_response,
-                   redirect,
-                   render_template,
-                   session,
-                   url_for,
-                   flash)
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 import unittest
 
-
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 
-to_do_list = ['Comprar café', 'ir a el banco', 'Compar fruta']
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
-class LoginFrom(FlaskForm):
-    user_name = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
 
 
@@ -33,38 +26,45 @@ def test():
     unittest.TextTestRunner().run(tests)
 
 
-@app.errorhandler(500)
-def not_found_500(error):
-    return render_template('500.html', error=error)
-
 @app.errorhandler(404)
-def not_found_404(error):
+def not_found(error):
     return render_template('404.html', error=error)
 
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
 
 
 @app.route('/')
 def index():
     user_ip = request.remote_addr
-    response = make_response((redirect('/hello')))
+
+    response = make_response(redirect('/hello'))
     session['user_ip'] = user_ip
+
     return response
 
 
 @app.route('/hello', methods=['GET', 'POST'])
-def hello_world():
+def hello():
     user_ip = session.get('user_ip')
-    username = session.get('useername')
-    login_form = LoginFrom()
-    context = dict(user_ip=user_ip,
-                   todos=to_do_list,
-                   login_form=login_form,
-                   username=username)
+    login_form = LoginForm()
+    username = session.get('username')
+
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+        'login_form': login_form,
+        'username': username
+    }
 
     if login_form.validate_on_submit():
-        username = login_form.user_name.data
-        session['useername'] = username
-        flash('Nombre de usuario registrado con éxito')
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con éxito!')
+
         return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
@@ -77,4 +77,3 @@ def index_pandas():
 
 if __name__ == '__main__':
     app.run()
-
