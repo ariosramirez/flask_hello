5638ec54103d21ce9be79302578188db513f9793
Andres David Rios Ramirez <andres.riosramirez@mercadolibre.com>
2020-03-10 17:17:12 -0300

    added auth view

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,26 @@
-from flask import render_template
+from flask import (render_template,
+                   redirect,
+                   url_for,
+                   session,
+                   flash)
 from app.forms import LoginForm
 
-from app.auth import auth
+from . import auth
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
     context = {
-        'login_form': login_form,
+        'login_form': login_form
     }
-    return render_template('login.html', **context)
 
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usario registrado con éxito!')
+
+        return redirect(url_for('index'))
+
+    return render_template('login.html', **context)
