e693470133d192ad1bd7aca39e556cffc34754d2
Andres David Rios Ramirez <andres.riosramirez@mercadolibre.com>
2020-09-13 13:43:10 -0300

    canged language and buttons

--- app/forms.py
+++ app/forms.py
@@ -6,16 +6,16 @@ from wtforms.validators import DataRequired
 class AuthForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
+    submit = SubmitField('Send')
 
 
 class TodoForm(FlaskForm):
     description = StringField('Descripción', validators=[DataRequired()])
-    submit = SubmitField('Crear')
+    submit = SubmitField('Create')
 
 
 class DeleteTodoForm(FlaskForm):
-    submit = SubmitField('Borrar')
+    submit = SubmitField('Delete')
 
 
 class UpdateTodoForm(FlaskForm):
